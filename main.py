52c71f5ed25b3052c678daeb955d0561c849e243
Shaikh Arish Iqbal <shaikharishiqbal@gmail.com>
2025-03-12 20:53:47 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+import cv2
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'webp', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.secret_key = 'super secret key'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    
+
+def processImage(filename, operation):
+    print(f"the operation is {operation} and filename is {filename}")
+    img = cv2.imread(f"uploads/{filename}")
+    match operation:
+        case "cgray":
+            imgProcessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(f"static/{filename}", imgProcessed)
+            return filename
+    pass
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        operation = request.form.get("operation")
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            processImage(filename, operation)
+            flash(f"Image processed successfully. You can download it from <a href='/static/{filename}' target='_blank' >here</a>.")
+            return render_template("index.html")
+        
+    return render_template("index.html")
+
+
+app.run(debug=True)
